0831c3c1f31bd9efa1acd9faa426fbe420dcfaa6
molenzwiebel <info@thijsmolendijk.nl>
2016-11-10 19:09:08 +0100

    Unify indentation.

--- payload/payload.cc
+++ payload/payload.cc
@@ -85,11 +85,11 @@ extern "C" {
     // CEF so we can run code just _before_ the first plugin is fetched. An environment
     // variable is used to determine which payload to inject, if any.
     cef_return_value_t CEF_CALLBACK on_before_resource_load(cef_request_handler_t* self, cef_browser_t* browser, cef_frame_t* frame, cef_request_t* request, cef_request_callback_t* callback) {
-	    static bool did_initial_inject = true;
+        static bool did_initial_inject = true;
         
         // We have to do this workaround to get from the LCU 16-bit CEF strings to
-	    // the 8-bit strings (char*s) that C++ uses. 
-	    cef_string_userfree_t url = request->get_url(request);
+        // the 8-bit strings (char*s) that C++ uses. 
+        cef_string_userfree_t url = request->get_url(request);
         cef_string_utf8_t str = {};
         cef_string_utf16_to_utf8(url->str, url->length, &str);
 
@@ -100,44 +100,42 @@ extern "C" {
             did_initial_inject = false;
         }
 
-	    // This is a crude but effective way to check if we are loading
-	    // what seems to be a plugin.
-	    if (strstr(str.str, "/fe/") && strstr(str.str, "/index.html") && LOAD_PAYLOAD) {
-		    // Read the code that has to be injected, and convert it to std::string.
+        // This is a crude but effective way to check if we are loading
+        // what seems to be a plugin.
+        if (strstr(str.str, "/fe/") && strstr(str.str, "/index.html") && LOAD_PAYLOAD) {
+            // Read the code that has to be injected, and convert it to std::string.
             std::ifstream fin(did_initial_inject ? LOAD_PAYLOAD : INITIAL_PAYLOAD);
             std::string code((std::istreambuf_iterator<char>(fin)), std::istreambuf_iterator<char>());
             
             // It doesn't matter what the previous value was, we did the initial inject now.
             did_initial_inject = true;
 
-		    // Convert the 8-bit string back to the CEF string type.
-		    cef_string_t js_str = {};
-		    cef_string_from_ascii(code.c_str(), code.length(), &js_str);
-		    frame->execute_java_script(frame, &js_str, url, 0);
-	    }
+            // Convert the 8-bit string back to the CEF string type.
+            cef_string_t js_str = {};
+            cef_string_from_ascii(code.c_str(), code.length(), &js_str);
+            frame->execute_java_script(frame, &js_str, url, 0);
+        }
 
-	    // Free the URL that we asked for earlier.
-	    cef_string_userfree_free(url);
+        // Free the URL that we asked for earlier.
+        cef_string_userfree_free(url);
 
         // Allow the request.
-	    return RV_CONTINUE;
+        return RV_CONTINUE;
     }
 
     // This is our version of the request_handler that we will inject into the CEF process.
     // We override this to simply gain access to the `on_before_resource_load` callback.
     // We query the old request handler, in case Riot has it set to something besides the default.
     cef_request_handler_t* CEF_CALLBACK get_request_handler(cef_client_t* self) {
-	    cef_request_handler_t null_handler = {};
-	    cef_request_handler_t* ret = old_request_handler ? old_request_handler(self) : &null_handler;
-	    ret->on_before_resource_load = on_before_resource_load;
+        cef_request_handler_t null_handler = {};
+        cef_request_handler_t* ret = old_request_handler ? old_request_handler(self) : &null_handler;
+        ret->on_before_resource_load = on_before_resource_load;
 
-	    return ret;
+        return ret;
     }
 
     // Called at the very start before any other cef_** functions are ran.
     // We use this to enable remote debugging and the ignoring of certificate errors.
-    // TODO(molenzwiebel): Change ignore_certificate_errors to a custom callback
-    // on the cef_request_handler_t struct instead.
     int CEF_EXPORT wrapped_cef_initialize(const cef_main_args_t* args, const cef_settings_t* settings, cef_app_t* application, void* windows_sandbox_info) {
         cef_settings_t* mutable_settings = (cef_settings_t*) settings;
         mutable_settings->remote_debugging_port = 8888;
@@ -152,10 +150,10 @@ extern "C" {
     // Called when a new browser host is created. We hook this to insert our
     // own request handler, which is then able to insert Ace before any plugin loads.
     int wrapped_cef_browser_host_create_browser(const cef_window_info_t* window_info, cef_client_t* client, const cef_string_t* url, const cef_browser_settings_t* settings, cef_request_context_t* request_context) {	
-	    old_request_handler = client->get_request_handler;
-	    client->get_request_handler = get_request_handler;
+        old_request_handler = client->get_request_handler;
+        client->get_request_handler = get_request_handler;
 
-	    return cef_browser_host_create_browser(window_info, client, url, settings, request_context);
+        return cef_browser_host_create_browser(window_info, client, url, settings, request_context);
     }
 
     // Interpose new function, does nothing on windows.
